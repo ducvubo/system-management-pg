1f752aaa68cc4d673ddd70a19f78c5b1c386deae
ducvubo <vminhduc8@gmail.com>
2025-06-27 23:08:33 +0700

    refactor: update Elasticsearch connection address in initialization

--- internal/initialize/elasticsearch.go
+++ internal/initialize/elasticsearch.go
@@ -33,7 +33,7 @@ func GetElasticsearchInstance() *elasticsearch.Client {
 // initElasticsearch initializes the Elasticsearch connection
 func (e *ElasticsearchSingleton) initElasticsearch() {
 	cfg := elasticsearch.Config{
-		Addresses: []string{"https://160.191.243.179:9200"},
+		Addresses: []string{"https://160.191.245.32:30808"},
 		Username:  "elastic",
 		Password:  "Duc17052003*",
 		Transport: &http.Transport{
